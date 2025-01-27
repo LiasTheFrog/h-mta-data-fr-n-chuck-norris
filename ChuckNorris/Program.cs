9d0c15661413a7b7d979a15c596f71896b0f3c1a
Lias <lias.israelsson@edu.lycksele.se>
2025-01-27 13:05:25 +0100

    first commit

---
+++ ChuckNorris/Program.cs
@@ -0,0 +1,33 @@
+﻿using System.Net.Http;
+
+
+
+
+class Program{
+
+static async void GetJoke(){
+using(HttpClient kalle = new HttpClient()){
+
+ kalle.BaseAddress = new Uri ("https://api.chucknorris.io");
+
+try{
+    HttpResponseMessage response =  await kalle.GetAsync("jokes/random");
+    response.EnsureSuccessStatusCode();
+
+    string responseBody = await response.Content.ReadAsStringAsync();
+    Console.WriteLine(responseBody);
+}
+catch(HttpRequestException e){
+    Console.WriteLine(e.Message);
+}
+}
+}
+static void Main(){
+    
+GetJoke();
+
+
+}
+
+}
+
